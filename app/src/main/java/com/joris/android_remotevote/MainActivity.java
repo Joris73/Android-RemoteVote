277bed4cd788530d479c60baa8221bd65af3f006
Joris <bodin.joris@gmail.com>
2016-01-28 10:47:32 +0100

    First commit

---
+++ app/src/main/java/com/joris/android_remotevote/MainActivity.java
@@ -0,0 +1,13 @@
+package com.joris.android_remotevote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
