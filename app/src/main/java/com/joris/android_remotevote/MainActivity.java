aeade7b5f3977f5e88a3d19506dbe48d57c92b98
Joris <bodin.joris@gmail.com>
2016-01-28 12:02:31 +0100

    Ajout des options

--- app/src/main/java/com/joris/android_remotevote/MainActivity.java
+++ app/src/main/java/com/joris/android_remotevote/MainActivity.java
@@ -1,13 +1,56 @@
 package com.joris.android_remotevote;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int RESULT_SETTINGS = 42;
+    private String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences sharedPrefs = PreferenceManager
+                .getDefaultSharedPreferences(this);
+
+        username = sharedPrefs.getString("prefUsername", "");
+        if (username.equals("")) {
+            launchPreference();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                launchPreference();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void launchPreference() {
+        Intent intent = new Intent(this, PreferencesActivity.class);
+        startActivityForResult(intent, RESULT_SETTINGS);
     }
 }
---
+++ app/src/main/java/com/joris/android_remotevote/PreferencesActivity.java
@@ -0,0 +1,29 @@
+package com.joris.android_remotevote;
+
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+/**
+ * Activité pour modifier les différentes options de l'application
+ */
+public class PreferencesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+    }
+
+    public static class MyPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(final Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+}
